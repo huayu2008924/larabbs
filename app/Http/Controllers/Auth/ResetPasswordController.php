ae20e4a5cdb9d0ff978c55f855c2fd0d5a09ccb0
huayu2008924 <huayu2008924@163.com>
2019-06-18 07:14:17 +0000

    找回密码成功消息提示

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
 class ResetPasswordController extends Controller
 {
@@ -36,4 +37,15 @@ class ResetPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+    /**
+     * 从代码以及其中的注释中可看出，成功时候调用了 sendResetResponse() 方法，
+     * 可惜此方法内的逻辑不是我们想要的。我们需要在表单提交成功后，设置闪存信息，再重定向到首页
+     * 我们可以利用 PHP 里 Trait 的加载机制，在控制器中重写 sendResetResponse() 方法
+     */
+    protected function sendResetResponse(Request $request, $response)
+    {
+        session()->flash('success','密码更新成功，您已成功修改密码！');
+        return redirect($this->redirectPath());
+    }
+
 }

1a4270d428b5d474d9c06958ef9c98a0dd70b536
huayu2008924 <huayu2008924@163.com>
2019-07-03 08:53:04 +0000

    翻译 URL 标示

--- routes/web.php
+++ routes/web.php
@@ -37,8 +37,10 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 //我们使用 Laravel 的 资源控制器 功能，接下来我们先给控制器注册一个资源路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 //上传图片
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
